921a9daf94eadc9a5e55aea4ce02e1b21f35bea2
Horace Velmont <dragond7@naver.com>
2020-04-14 23:36:52 +0900

    B1038 failed

---
+++ src/main/kotlin/baekjoon/B1038.kt
@@ -0,0 +1,39 @@
+package baekjoon
+
+import java.util.*
+import kotlin.math.pow
+
+fun main() = with(Scanner(System.`in`)) {
+    val N = nextInt()
+    var num = 0
+    var cnt = -1
+    val cache = BooleanArray(100)
+    fun isDescendent(num: Int): Boolean {
+        val str = num.toString()
+        var flag = true
+
+        for (i in 0 until str.length-1) {
+            if (cache((str[i] + str[i+1]).toInt()))
+            if (str[0] <= str[1]) {
+                flag = false
+            }
+            if (flag && str.length > 2) {
+                val pow = (10.0.pow(str.length)).toInt()
+                val leftShift = num - (num / pow) * pow;
+                flag = if (leftShift < 10) false else cache[leftShift]
+            }
+        }
+        return flag
+    }
+
+    var sol = 0
+    while (cnt < N) {
+        if (isDescendent(num)) {
+            cache[num] = true
+            cnt += 1
+            sol = num
+        }
+        num += 1
+    }
+    print(sol)
+}
