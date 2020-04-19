856a362950a527055bd656f69ff28605daa60e7b
Horace Velmont <dragond7@naver.com>
2020-04-20 01:16:10 +0900

    solve B1012

--- src/main/kotlin/baekjoon/B1012.kt
+++ src/main/kotlin/baekjoon/B1012.kt
@@ -6,41 +6,43 @@ fun main() = with(Scanner(System.`in`)) {
     val dx = intArrayOf(1, -1, 0, 0)
     val dy = intArrayOf(0, 0, 1, -1)
 
-    fun getScore(M: Int, N: Int, cbgArr: Array<Pair<Int, Int>>): Int {
-        fun search(x: Int, y: Int, box: Array<IntArray>, chk: Array<BooleanArray>): Boolean {
-            if (box[y][x] == 1 && !chk[y][x]) {
-                chk[y][x] = true
-                for (i in 0..3) {
-                    search(x + dx[i], y + dy[i], box, chk)
-                }
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        val box = Array(N) { IntArray(M) }
-        val chk = Array(N) { BooleanArray(M) }
-        var cnt = 0;
-        cbgArr.forEach { p -> box[p.second][p.first] == 1 };
-
-        for (y in box.indices) {
-            for (x in box[0].indices) {
-                if (search(x, y, box, chk)) {
-                    cnt += 1
-                    println("cnt : ${cnt}")
-                }
-            }
-        }
-        return cnt;
-    }
-
     val T = nextInt()
     for (i in 0 until T) {
         val M = nextInt()
         val N = nextInt()
         val K = nextInt()
-        val cbgArr = Array(K) { Pair(nextInt(), nextInt()) }
-        println(getScore(M, N, cbgArr))
+
+        fun getScore(M: Int, N: Int, K: Int): Int {
+            fun search(x: Int, y: Int, box: Array<BooleanArray>): Boolean {
+                if (box[y][x]) {
+                    box[y][x] = false
+                    for (i in 0..3) {
+                        if (x + dx[i] in 0 until M && y + dy[i] in 0 until N) {
+                            search(x + dx[i], y + dy[i], box)
+                        }
+                    }
+
+                    return true;
+                }
+                return false
+            }
+
+            val box = Array(N) { BooleanArray(M) }
+            for (j in 0 until K) {
+                val x = nextInt()
+                val y = nextInt()
+                box[y][x] = true
+            }
+
+            var cnt = 0;
+            for (y in box.indices) for (x in box[0].indices) {
+                if (search(x, y, box)) {
+                    cnt += 1
+                }
+            }
+
+            return cnt;
+        }
+        println(getScore(M, N, K))
     }
 }
